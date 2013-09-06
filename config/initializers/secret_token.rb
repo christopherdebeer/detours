aaa3014ddef239ab89b1bae61d8a5d942d89cb29
Christopher de Beer <christopherdebeer@redxiii.config>
2013-09-06 22:22:54 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Detours::Application.config.secret_token = 'ff4dd6531391738ecbc2db42291989bfe17692bfe43bd92b0b55116b4edfbdd3cad5d134bdf2df4163b5e8f9f328914efd5af42eeda89d088ceb74fcc08c100d'
